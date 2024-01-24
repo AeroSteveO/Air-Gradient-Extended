1b4ea0a1bfcddd39b1c96f7eac1e8719baae6953
stephen <thesteveo64@gmail.com>
2024-01-23 19:52:47 -0600

    trying to sync up input and output variables

--- esphome/components/lps22_sensor/lps22_sensor.h
+++ esphome/components/lps22_sensor/lps22_sensor.h
@@ -15,8 +15,8 @@ namespace lps22_sensor {
 
 class LPS22Sensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
  public:
-  void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
-  void set_pressure_sensor(sensor::Sensor *pressure_sensor) { pressure_sensor_ = pressure_sensor; }
+  void set_temperature_sensor(sensor::Sensor *temperature_sensor_) { temperature_sensor = temperature_sensor_; }
+  void set_pressure_sensor(sensor::Sensor *pressure_sensor_) { pressure_sensor = pressure_sensor_; }
   
   Adafruit_LPS22 lps;
   Sensor *temperature_sensor = new Sensor();
